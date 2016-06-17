897b4df1570875a8f3302784eb57b5cbeaf3cf3b
Vladimir Trukhin <vvt@narod.ru>
2016-06-17 21:54:19 +0400

    Simple pipe example

--- CPP_Multithreading/Module5/5.4_Pipes/pipes.c
+++ CPP_Multithreading/Module5/5.4_Pipes/pipes.c
@@ -3,25 +3,25 @@
 
 void who_wc() {
     int pfd[2];
-    pipe(pfd);
-    if(!fork()) {
-        close(STDOUT_FILENO);
-        dup2(pfd[1], STDOUT_FILENO);
-        close(pfd[1]);
-        close(pfd[0]);
-        execlp("who", "who", NULL);
+    pipe( pfd );
+    if( !fork() ) {
+        close( STDOUT_FILENO );
+        dup2( pfd[1], STDOUT_FILENO );
+        close( pfd[1] );
+        close( pfd[0] );
+        execlp( "who", "who", NULL );
     } else {
-        close(STDIN_FILENO);
-        dup2(pfd[0], STDIN_FILENO);
-        close(pfd[1]);
-        close(pfd[0]);
-        execlp("wc", "wc", "-l", NULL);
-        // wait(NULL);
-        // wait(NULL);
+        close( STDIN_FILENO );
+        dup2( pfd[0], STDIN_FILENO );
+        close( pfd[1] );
+        close( pfd[0] );
+        execlp( "wc", "wc", "-l", NULL );
+        // wait( NULL );
+        // wait( NULL );
         }
 }
 
-int main (int argc, char ** argv) {
+int main ( int argc, char ** argv ) {
     who_wc();
     return 0;
 }
