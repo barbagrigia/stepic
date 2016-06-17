6db96cdc02c8804538b051cb82727e9f7b91e017
Vladimir <vvt@narod.ru>
2016-06-17 16:00:54 +0300

    initial

--- CPP_Multithreading/Module5/5.4_Pipes/pipes/shell.cpp
+++ CPP_Multithreading/Module5/5.4_Pipes/pipes/shell.cpp
@@ -1,83 +1,87 @@
 #include <iostream>
+#include <stdio.h>
+#include <string>
 #include <vector>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <wait.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
 
-using std::vector;
-using std::string;
-using cmd_list = vector<vector<string>>;
+using namespace std;
 
-cmd_list get_commands() {
-    string s;
-    vector<string> cmd;
-    cmd_list lst;
-    while(std::cin >> s) {
-        if(s == "|") {
-            if(cmd.size() > 0)
-                lst.push_back(cmd);
-            cmd.clear();
-        } else {
-            cmd.push_back(s);
-        }
-    }
-    if(cmd.size() > 0)
-        lst.push_back(cmd);
+int execute_child( const vector<string>& path, int pipe_input = -1 )
+{
+	int pfd[2];
+	pipe( pfd );
+	
 
-    return lst;
+	int child_pid = fork();
+	if( !child_pid ) {
+		char*  argv[64];
+		int i = 0;
+		for( ; i < path.size(); i++ ) {
+			argv[i] = const_cast<char*>( path[i].c_str() );
+		}
+		argv[i] = 0;
+	
+		if( pipe_input != -1 ) {		
+			close( STDIN_FILENO );
+			dup2( pipe_input, STDIN_FILENO );
+			close( pipe_input );
+		}
+		close( STDOUT_FILENO );
+		dup2( pfd[1], STDOUT_FILENO );
+		close( pfd[0] );
+		close( pfd[1] );
+		execvp( path[0].c_str(), argv );
+		exit( 0 );
+		return -1;
+	} else {
+		close( pfd[1] );
+		waitpid( child_pid, 0, 0 );
+		return pfd[0];
+	}
 }
 
-void execute(const cmd_list& lst) {
-    int p_out = STDIN_FILENO;
-
-    for(int cmd_i =0; cmd_i<lst.size(); ++cmd_i) {
-        const cmd_list::value_type& cmd = lst[cmd_i];
-
-        char** params = new char*[cmd.size()+1];
-        for(int i =0; i< cmd.size(); ++i)
-            params[i] = strdup(cmd[i].c_str());
-        params[cmd.size()] = NULL;
-
-        int pfd[2];
-        pipe(pfd);
-
-        if(cmd_i != lst.size() - 1) {
-            if(!fork()) {
-                close(STDOUT_FILENO);
-                if (p_out != STDIN_FILENO)
-                    close(STDIN_FILENO);
-                dup2(p_out, STDIN_FILENO);
-                dup2(pfd[1], STDOUT_FILENO);
-                close(p_out);
-                close(pfd[0]);
-                close(pfd[1]);
-                execvp(params[0],params);
-            }
-        } else {
-            if (p_out != STDIN_FILENO)
-                close(STDIN_FILENO);
-            dup2(p_out, STDIN_FILENO);
-            close(p_out);
-            close(pfd[0]);
-            close(pfd[1]);
-            execvp(params[0],params);
-        }
-        
-        for(int i=0; i< cmd.size(); ++i)
-            free(params[i]);
-        delete params;
-        p_out = pfd[0];
-        close(pfd[1]);
-    }
-
-    int status;
-    while(waitpid(-1, &status, WNOHANG) != -1);
+void out_result( int pipe_input )
+{
+	char buffer[4096];
+	size_t len = 0;
+	FILE* file = fopen( "/home/box/result.out", "w" );
+	while( (len = read( pipe_input, buffer, sizeof( buffer ) ) ) > 0  ) {
+		size_t len2 = fwrite( buffer, 1, len, file );
+		if( len2 < len ) {
+			printf( "Copy error\n" );
+			return;
+		}
+	}
 }
 
-int main() {
-    cmd_list lst = get_commands();
-    execute(lst);
+int main(int, char** )
+{
+	string input;
+	getline( cin, input );
 
-    return 0;
+	int pipe_input = -1;	
+	int pipe_pos = 0;
+	while( pipe_pos != string::npos ) {
+		string command;
+		pipe_pos = input.find( '|' );
+		if( pipe_pos == string::npos ) {
+			command = input;
+		} else {
+			command = input.substr( 0, pipe_pos );
+			input = input.substr( pipe_pos + 1 );
+		}
+		istringstream iss( command );
+		vector<string> commands( istream_iterator<string>{iss},
+					istream_iterator<string>{} );
+		pipe_input = execute_child( commands, pipe_input );
+	}
+	
+	out_result( pipe_input );		
+	close( pipe_input );
+	return 0;
 }
