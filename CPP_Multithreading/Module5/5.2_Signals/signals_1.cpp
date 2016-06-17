bfe2aa7a1a5c7ffd7b46db26162f1f42b7d0b498
Vladimir <vvt@narod.ru>
2016-06-17 12:38:33 +0300

    add Module5

---
+++ CPP_Multithreading/Module5/5.2_Signals/signals_1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>                                                                                      
+#include <stdlib.h>                                                                                     
+#include <unistd.h>                                                                                     
+#include <sys/types.h>                                                                                  
+#include <signal.h>                                                                                     
+#include <wait.h>                                                                                       
+#include <string.h>                                                                                     
+
+int main() { 
+		pid_t pid = getpid();
+		FILE *f = fopen("/home/box/pid", "w");
+		fprintf(f, "%d", pid);
+		fclose(f);
+		
+		signal(SIGTERM, SIG_IGN);
+		signal(SIGINT, SIG_IGN);
+		
+		while(!done) {
+			pause();
+        }
+}
