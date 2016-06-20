bdf9684f410e3c66b8d8bbb6b737c8031aa7a09f
Vladimir Trukhin <vvt@narod.ru>
2016-06-20 21:20:59 +0400

    Update sp_mapper.py

--- Hadoop/Module5/5.2/sp_mapper.py
+++ Hadoop/Module5/5.2/sp_mapper.py
@@ -8,9 +8,9 @@ for line in sys.stdin:
     print('{0}\t{1}\t{2}'.format(v, w, lst))
     
     for ve in strip_lst(lst):
-        if ve=='':
+        if ve == '':
             continue
-        if w!='INF':
+        if w != 'INF':
             print('{0}\t{1}\t{2}'.format(ve, int(w)+1, '{}'))
         else:
             print('{0}\tINF\t{1}'.format(ve, '{}'))
