fe4968897e9b1efb75d86a1a85b5524c01c84ff9
Vladimir Trukhin <vvt@narod.ru>
2016-06-20 21:16:11 +0400

    Shortest path mapper

---
+++ Hadoop/Module5/5.2/sp_mapper.py
@@ -0,0 +1,16 @@
+def strip_lst(lst):
+    return lst[1:len(lst)-1].strip().split(',')
+
+import sys
+for line in sys.stdin:
+    v, w, lst = line.strip().split("\t")
+    print('{0}\t{1}\t{2}'.format(v, w, lst))
+    
+    
+    for ve in strip_lst(lst):
+        if ve=='':
+            continue
+        if w!='INF':
+            print('{0}\t{1}\t{2}'.format(ve, int(w)+1, '{}'))
+        else:
+            print('{0}\tINF\t{1}'.format(ve, '{}'))
