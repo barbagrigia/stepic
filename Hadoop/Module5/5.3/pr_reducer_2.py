eb5229e8db52f3ec02301122d403069bcfc76481
Vladimir Trukhin <vvt@narod.ru>
2016-06-20 22:31:12 +0400

    PageRank reducer

---
+++ Hadoop/Module5/5.3/pr_reducer_2.py
@@ -0,0 +1,23 @@
+import sys
+
+prev_v = None
+prev_w = 0.0
+prev_adj = '{}'
+
+for line in sys.stdin:
+    v, w, lst = line.strip().split("\t")                
+        
+    if prev_v and v != prev_v:
+        weight = prev_w * 0.9 + 0.02
+        print('{0}\t{1:.3f}\t{2}'.format(prev_v, weight, prev_adj));
+        prev_w = 0.0
+        prev_adj = '{}'
+    
+    if lst != '{}':
+        prev_adj = lst;
+    else:
+        prev_w += float(w)
+    prev_v =  v
+        
+weight = prev_w * 0.9 + 0.02
+print('{0}\t{1:.3f}\t{2}'.format(prev_v, weight, prev_adj));
